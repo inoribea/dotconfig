2af163eff55fe0e37ff4f3a5bd3cd6cd261a9dec
inoribea <inoribea@outlook.com>
2024-10-09 13:49:52 +0800

    update

---
+++ dotconfig.sh
@@ -0,0 +1,8 @@
+cd /home/inoribea/dotconfig
+echo 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
+git remote set-url origin "https://inoribea@github.com/inoribea/dotconfig.git"
+sleep 1
+git add .
+git commit -m update
+git push origin +master
+cd /home/inoribea
---
+++ fish/config.fish
@@ -0,0 +1,47 @@
+#set -g fish_greeting
+
+function fish_greeting
+     pokemon-colorscripts -r
+end
+
+function starship_transent_rmpt_func
+  starship module time
+end
+starship init fish | source
+enable_transience
+
+
+if status is-interactive
+    starship init fish | source
+end
+
+# Alias
+alias fetch="fastfetch"
+alias lol="lolcat"
+alias linkwd="sudo mount --bind /home/inoribea/Documents /home/inoribea/.local/share/waydroid/data/media/0/Documents"
+alias uphyde="cd ~/HyDE/Scripts
+git pull
+./install.sh -r
+cp -rf /home/inoribea/dotconfig/* /home/inoribea/.config"
+alias upconf="sh /home/inoribea/dotconfig/dotconfig.sh"
+alias dnsoff="sudo systemctl stop dnsproxy
+sleep 0.5
+sudo pkill -f dnsproxy
+sudo systemctl start systemd-resolved"
+alias dnson="sudo systemctl stop systemd-resolved
+sleep 0.5
+sudo pkill -f systemd-resolved
+sudo systemctl start dnsproxy"
+alias lsp="sudo lsof -nP -iTCP -sTCP:LISTEN"
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
+alias ssh='kitten ssh'
