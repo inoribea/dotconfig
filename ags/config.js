8ede471cd77fdf955e9028c07bccaec9abaa955e
inoribea <inoribea@outlook.com>
2024-11-10 18:28:36 +0800

    update

--- ags/config.js
+++ ags/config.js
@@ -1,40 +1,5 @@
-"use strict";
-// Import
-import Gdk from 'gi://Gdk';
-import GLib from 'gi://GLib';
-//import App from 'resource:///com/github/Aylur/ags/app.js'
-//import * as Utils from 'resource:///com/github/Aylur/ags/utils.js'
-// Stuff
-import userOptions from './modules/.configuration/user_options.js';
-import { firstRunWelcome, startBatteryWarningService } from './services/messages.js';
-import { startAutoDarkModeService } from './services/darkmode.js';
-// Widgets
-import { Bar, BarCornerTopleft, BarCornerTopright } from './modules/bar/main.js';
-import Cheatsheet from './modules/cheatsheet/main.js';
-// import DesktopBackground from './modules/desktopbackground/main.js';
-import Dock from './modules/dock/main.js';
-import Corner from './modules/screencorners/main.js';
-import Crosshair from './modules/crosshair/main.js';
-import Indicator from './modules/indicators/main.js';
-import Osk from './modules/onscreenkeyboard/main.js';
-import Overview from './modules/overview/main.js';
-import Session from './modules/session/main.js';
-import SideLeft from './modules/sideleft/main.js';
-import SideRight from './modules/sideright/main.js';
-import { COMPILED_STYLE_DIR } from './init.js';
-
-const range = (length, start = 1) => Array.from({ length }, (_, i) => i + start);
-function forMonitors(widget) {
-    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
-    return range(n, 0).map(widget).flat(1);
-}
-function forMonitorsAsync(widget) {
-    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
-    return range(n, 0).forEach((n) => widget(n).catch(print))
-}
-
 import { Utils, App, Notifications } from './modules/utils/imports.js';
-//import { Bar } from './modules/topbar.js';
+import { Bar } from './modules/topbar.js';
 import { MainMenu } from './modules/menus/MainMenu.js';
 import { VolumeOSD } from './modules/on-screen/volume.js';
 import MyNotifications from './modules/notifications/OSDNotifications.js';
@@ -60,50 +25,75 @@ import {
 } from './modules/components/ScreenCorners.js';
 import { languageLayoutOSD } from './modules/on-screen/KeyboardLayout.js';
 
+import userOptions from './modules/.configuration/user_options.js';
+import Indicator from './modules/indicators/main.js';
+import Overview from './modules/overview/main.js';
+import Session from './modules/session/main.js';
+import SideLeft from './modules/sideleft/main.js';
+import SideRight from './modules/sideright/main.js';
+import { COMPILED_STYLE_DIR } from './init.js';
 
-// Start stuff
-handleStyles(true);
-startAutoDarkModeService().catch(print);
-firstRunWelcome().catch(print);
-startBatteryWarningService().catch(print)
+// in config.js
+const scss = App.configDir + '/scss/main.scss';
+const css = App.configDir + '/style.css';
 
-const Windows = () => [
-    // forMonitors(DesktopBackground),
-    forMonitors(Crosshair),
+Utils.exec(`sassc ${scss} ${css}`);
+
+let windows = [
+    VolumeOSD(),
+    MyNotifications(),
+    PrayerTimesMenu(),
+    CalendarMenu(),
+    languageLayoutOSD,
     Overview(),
-    forMonitors(Indicator),
-    forMonitors(Cheatsheet),
     SideLeft(),
     SideRight(),
-    forMonitors(Osk),
-    forMonitors(Session),
-    ...(userOptions.dock.enabled ? [forMonitors(Dock)] : []),
-    ...(userOptions.appearance.fakeScreenRounding !== 0 ? [
-        forMonitors((id) => Corner(id, 'top left', true)),
-        forMonitors((id) => Corner(id, 'top right', true)),
-        forMonitors((id) => Corner(id, 'bottom left', true)),
-        forMonitors((id) => Corner(id, 'bottom right', true)),
-    ] : []),
-    ...(userOptions.appearance.barRoundCorners ? [
-        forMonitors(BarCornerTopleft),
-        forMonitors(BarCornerTopright),
-    ] : []),
+    // ... Desktop widgets ... //
+    ColorWidget,
+    win20Widget,
+    materialWidget,
+    unicatWidget,
+    blackHoleWidget,
+    goldenWidget,
+    harmonyWidget,
+    newCatWidget,
+    deerWidget,
+    circlesMusicWidget,
+    whiteFlowerWidget,
+    applauncher,
 ];
 
-const CLOSE_ANIM_TIME = 210; // Longer than actual anim time to make sure widgets animate fully
-const closeWindowDelays = {}; // For animations
-for (let i = 0; i < (Gdk.Display.get_default()?.get_n_monitors() || 1); i++) {
-    closeWindowDelays[`osk${i}`] = CLOSE_ANIM_TIME;
+const screens = JSON.parse(Utils.exec('hyprctl monitors all -j'));
+
+for (let i = 0; i < screens.length; i++) {
+    const screen = screens[i];
+
+    windows.push(Bar({ monitor: screen.id }));
+
+    const leftMene = MainMenu({ monitor: screen.id });
+    windows.push(leftMene);
+    windows.push(TopLeftCorner({ monitor: screen.id }));
+    windows.push(TopRightCorner({ monitor: screen.id }));
 }
 
-//App.config({
-//    css: `${COMPILED_STYLE_DIR}/style.css`,
-//    stackTraceOnError: true,
-//    closeWindowDelay: closeWindowDelays,
-//    windows: Windows().flat(1),
-//});
+Notifications.cacheActions;
+globalThis.getNot = () => Notifications;
 
-// Stuff that don't need to be toggled. And they're async so ugh...
-forMonitorsAsync(Bar);
-// Bar().catch(print); // Use this to debug the bar. Single monitor only.
+Utils.execAsync([`paplay`, settings.assets.audio.desktop_login]).catch(print);
 
+Utils.execAsync([
+    'python',
+    settings.scripts.createThumbnail,
+    settings.theme.darkM3WallpaperPath,
+]).catch(print);
+
+Utils.execAsync([
+    'python',
+    settings.scripts.createThumbnail,
+    settings.theme.lightM3WallpaperPath,
+]).catch(print);
+
+App.config({
+    css: css,
+    windows: windows,
+});
